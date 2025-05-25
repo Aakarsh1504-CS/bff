8b2ad9f3ceb0f647974c5fe746117bde693aa43c
Aakarsh1504-CS <aroraaakarsh0@gmail.com>
2025-05-25 15:09:24 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const kycRoutes = require('./routes/kyc.routes');
+const cors = require('cors');
+
+app.use(express.json());
+app.us(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const nonProtectedRoutes = [
+    '/v1/auth/login',
+    '/v1/auth/register',
+    '/v1/auth/forgot-password',
+    '/v1/auth/reset-password',
+];
+
+app.use((req, res, next) => {
+    //  have an authentication middleware here
+    if (nonProtectedRoutes.includes(req.path)) {
+        next();
+    }
+});
+
+app.get('/v1/auth/login', (req, res, next) => {
+    res.send('Login endpoint');
+});
+app.get('/v1/auth/register', (req, res, next) => {
+    res.send('Register endpoint');
+});
+app.get('/v1/auth/forgot-password', (req, res, next) => {
+    res.send('Forgot Password endpoint');
+});
+app.get('/v1/auth/reset-password', (req, res, next) => {
+    res.send('Reset Password endpoint');
+});
+
+app.use("/v1/kyc", kycRoutes)
+module.exports = app; 
